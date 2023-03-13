39b973ab92b0268d19299bd90094303055ba1f2e
mohammed basil <basilsilu38@gmail.com>
2023-03-14 00:53:34 +0530

    created reasons section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './assets/App.css'
 import Hero from './Components/Hero_section/Hero';
 import Programs from './Components/Programs/Programs';
+import Reasons from './Components/Reasons/Reasons';
 function App() {
   return (
     <div className="App">
           <Hero/>
           <Programs/>
+          <Reasons/>
     </div>
   );
 }
