53532dbf571449a4cf48dfc2df6d50b9adb95437
mohammed basil <basilsilu38@gmail.com>
2023-03-14 18:08:41 +0530

    created footer section

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Footer.css";
+import github from "../../assets/github.png";
+import instagram from "../../assets/instagram.png";
+import linkedin from "../../assets/linkedin.png";
+import logo from "../../assets/logo.png";
+
+const Footer = () => {
+  return (
+    <div className="footer_container">
+      <hr />
+      <div className="footer">
+        <div className="social_links">
+        <img src={github} alt="" />
+        <img src={instagram} alt="" />
+        <img src={linkedin} alt="" />
+      </div>
+      <div className="logo_footer">
+        <img src={logo} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
